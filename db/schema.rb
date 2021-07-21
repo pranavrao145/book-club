3739b6f00c2c4cc05d95817a79153bbd51061ece
Pranav Rao <pranavrao145@gmail.com>
2021-07-21 15:25:56 -0400

    added migrations to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_21_000459) do
+ActiveRecord::Schema.define(version: 2021_07_21_001358) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "google_books_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.string "title"
+    t.string "content"
+    t.integer "book_id", null: false
+    t.integer "author_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_reviews_on_author_id"
+    t.index ["book_id"], name: "index_reviews_on_book_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +42,6 @@ ActiveRecord::Schema.define(version: 2021_07_21_000459) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reviews", "authors"
+  add_foreign_key "reviews", "books"
 end
