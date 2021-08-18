56f138ab6c442fbb403c4529ae1bee036c30eb8f
Pranav Rao <pranavrao145@gmail.com>
2021-08-18 19:57:19 -0400

    added limits to discussion

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -4,6 +4,6 @@ class Discussion < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :replies, dependent: :destroy
 
-  validates :title, presence: true
-  validates :content, presence: true
+  validates :title, presence: true, length: { minimum: 0, maxmium: 100 }
+  validates :content, presence: true, length: { minimum: 0, maxmium: 1000 }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -4,6 +4,6 @@ class Review < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
 
   validates :rating, presence: true
-  validates :title, presence: true, length: {minimum: 0, maxmium: 100}
-  validates :content, length: {minimum: 0, maxmium: 1000}
+  validates :title, presence: true, length: { minimum: 0, maxmium: 100 }
+  validates :content, length: { minimum: 0, maxmium: 1000 }
 end
